9efa7b7a04d010835dd80c0f5cd5194434f70bdf
Boris Mikic <boris.blizzard@gmail.com>
2010-07-27 09:55:55 +0000

    - added main.h

---
+++ include/april/main.h
@@ -0,0 +1,15 @@
+/************************************************************************************\
+This source file is part of the Awesome Portable Rendering Interface Library         *
+For latest info, see http://libapril.sourceforge.net/                                *
+**************************************************************************************
+Copyright (c) 2010 Kresimir Spes (kreso@cateia.com)                                  *
+*                                                                                    *
+* This program is free software; you can redistribute it and/or modify it under      *
+* the terms of the BSD license: http://www.opensource.org/licenses/bsd-license.php   *
+\************************************************************************************/
+#ifndef APRIL_MAIN_H
+#define APRIL_MAIN_H
+
+//2DO
+
+#endif
