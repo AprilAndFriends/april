3b7d4efc9e2950635c388773cea313e63df48fb6
Boris Mikic <boris.blizzard@gmail.com>
2010-08-10 12:10:29 +0000

    - fixed a small bug

--- rendersystems/DirectX9/RenderSystem_DirectX9.h
+++ rendersystems/DirectX9/RenderSystem_DirectX9.h
@@ -73,7 +73,7 @@ namespace April
 		void triggerMouseUpEvent(int button);
 		void triggerMouseDownEvent(int button);
 		void triggerMouseMoveEvent();
-		void triggerQuitEvent();
+		bool triggerQuitEvent();
 		
 		void enterMainLoop();
 		void terminateMainLoop();
