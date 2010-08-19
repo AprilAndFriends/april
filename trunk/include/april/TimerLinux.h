cd42c7f2922b153e6be3da3cf0cd6496df2ef403
dcerjan <dcerjan@a048fc06-0db0-1d88-0edd-9057f1fe799b>
2010-08-19 12:02:41 +0000

    * added TimerLinux.h & TimerWin.h * TimerWin implemented

--- trunk/include/april/TimerLinux.h
+++ trunk/include/april/TimerLinux.h
@@ -7,13 +7,9 @@ Copyright (c) 2010 Kresimir Spes (kreso@cateia.com)                             
 * This program is free software; you can redistribute it and/or modify it under      *
 * the terms of the BSD license: http://www.opensource.org/licenses/bsd-license.php   *
 \************************************************************************************/
-#ifndef APRIL_TIMER_H
-#define APRIL_TIMER_H
+#ifndef APRIL_TIMER_LINUX_H
+#define APRIL_TIMER_LINUX_H
 
-#ifdef _WIN32
-    #include "TimerWin.h"
-#else
-    #include "TimerLinux.h"
-#endif
+#include "TimerWin.h"
 
 #endif
--- trunk/include/april/TimerWin.h
+++ trunk/include/april/TimerWin.h
@@ -7,13 +7,38 @@ Copyright (c) 2010 Kresimir Spes (kreso@cateia.com)                             
 * This program is free software; you can redistribute it and/or modify it under      *
 * the terms of the BSD license: http://www.opensource.org/licenses/bsd-license.php   *
 \************************************************************************************/
-#ifndef APRIL_TIMER_H
-#define APRIL_TIMER_H
+#ifndef APRIL_TIMER_WIN_H
+#define APRIL_TIMER_WIN_H
 
-#ifdef _WIN32
-    #include "TimerWin.h"
-#else
-    #include "TimerLinux.h"
-#endif
+#include <windows.h>
+#include <stdio.h>
+#include "AprilExport.h"
+
+namespace April
+{
+    class AprilExport Timer
+    {
+        
+        float mDt, mTd, mTd2;
+        __int64       mFrequency;
+        float         mResolution;
+        unsigned long mMmTimerStart;
+        unsigned long mMmTimerElapsed;
+        bool		  mPerformanceTimer;
+        __int64       mPerformanceTimerStart;
+        __int64       mPerformanceTimerElapsed;
+        
+        
+    public:
+        
+        Timer();
+        ~Timer();
+        
+        float getTime();
+        float diff(bool update = true);
+        
+        void update();
+    };
+}
 
 #endif
