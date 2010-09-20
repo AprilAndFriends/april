b02bc155bd9796edef8ae32e76e11a3e5053d7aa
ivucica <ivucica@gmail.com>
2010-09-20 12:27:36 +0000

    * began adding actual iOS code * began adding demo_simple-ios

---
+++ windowsystems/iOS/EAGLView.h
@@ -0,0 +1,48 @@
+/************************************************************************************\
+ This source file is part of the Awesome Portable Rendering Interface Library         *
+ For latest info, see http://libapril.sourceforge.net/                                *
+ **************************************************************************************
+ Copyright (c) 2010 Ivan Vucica (ivan@vucica.net)                                     *
+ *                                                                                    *
+ * This program is free software; you can redistribute it and/or modify it under      *
+ * the terms of the BSD license: http://www.opensource.org/licenses/bsd-license.php   *
+ \************************************************************************************/
+
+
+
+#import <UIKit/UIKit.h>
+#import <OpenGLES/EAGL.h>
+#import <OpenGLES/ES1/gl.h>
+#import <OpenGLES/ES1/glext.h>
+
+/*
+This class wraps the CAEAGLLayer from CoreAnimation into a convenient UIView subclass.
+The view content is basically an EAGL surface you render your OpenGL scene into.
+Note that setting the view non-opaque will only work if the EAGL surface has an alpha channel.
+*/
+@interface EAGLView : UIView {
+    
+@private
+    /* The pixel dimensions of the backbuffer */
+    GLint backingWidth;
+    GLint backingHeight;
+    
+    EAGLContext *context;
+    
+    /* OpenGL names for the renderbuffer and framebuffers used to render to this view */
+    GLuint viewRenderbuffer, viewFramebuffer;
+    
+    /* OpenGL name for the depth buffer that is attached to viewFramebuffer, if it exists (0 if it does not exist) */
+    GLuint depthRenderbuffer;
+    
+    NSTimer *animationTimer;
+    NSTimeInterval animationInterval;
+}
+
+@property NSTimeInterval animationInterval;
+
+- (void)startAnimation;
+- (void)stopAnimation;
+- (void)drawView;
+
+@end
