f9fe52c7709677d96417566ee387b1dbea362bf6
ivucica <ivucica@gmail.com>
2010-09-20 13:42:42 +0000

    * added uiapplicationdelegate * added ios sdk production * began getting demo_simple to use sdk and work

---
+++ windowsystems/iOS/AprilUIKitDelegate.m
@@ -0,0 +1,21 @@
+/************************************************************************************\
+ This source file is part of the Awesome Portable Rendering Interface Library         *
+ For latest info, see http://libapril.sourceforge.net/                                *
+ **************************************************************************************
+ Copyright (c) 2010 Ivan Vucica (ivan@vucica.net)                                     *
+ *                                                                                    *
+ * This program is free software; you can redistribute it and/or modify it under      *
+ * the terms of the BSD license: http://www.opensource.org/licenses/bsd-license.php   *
+ \************************************************************************************/
+
+#import "AprilUIKitDelegate.h"
+
+
+@implementation AprilUIKitDelegate
+
+@synthesize window;
+
+- (void)applicationDidFinishLaunching:(UIApplication *)application {
+	NSLog(@"Launched april app");
+}
+@end
