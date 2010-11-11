ef283be8d3175ae5f35256e1f107ed629b946a6c
ivucica <ivucica@gmail.com>
2010-11-11 14:17:00 +0000

    Added support code for APNs. Added basic implementation of iOS messagebox (not returning correct button, and possibly nonblocking)

--- windowsystems/iOS/AprilUIKitDelegate.h
+++ windowsystems/iOS/AprilUIKitDelegate.h
@@ -10,11 +10,29 @@
 
 #import <UIKit/UIKit.h>
 
+// we could define this as returning "void", and use NSData* or
+// char* for success argument, or NSError* or int+char* for the
+// failure argument. however, future iOS releases, or even some
+// completely different platform, might implement same handler,
+// yet require different arguments.
+//
+// hence, void* for return value and void* for the argument can
+// help keep the callback future proof. is that smart? dunno :)
+//
+// idea: this could perhaps be refactored as a "generic" 
+//       callback?
+// idea: should we nevertheless switch to platform-specific
+//       C-based handlers? 
+typedef void*(*AprilRemoteNotificationsHandler_t)(void *data);
+
 
 @interface AprilUIKitDelegate : NSObject<UIApplicationDelegate> {
 	UIWindow *window;
+	AprilRemoteNotificationsHandler_t onSuccess;
+	AprilRemoteNotificationsHandler_t onFailure;
 }
 
-@property (readwrite, retain) UIWindow *window;
-
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, assign) AprilRemoteNotificationsHandler_t onSuccess;
+@property (nonatomic, assign) AprilRemoteNotificationsHandler_t onFailure;
 @end
