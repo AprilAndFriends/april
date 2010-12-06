cefdf7639d1a2ab0e06d0796a4c3d2224df7c74b
ivucica <ivucica@gmail.com>
2010-12-06 13:21:00 +0000

    Made code compatible with iOS 3.x once again.

--- windowsystems/iOS/AprilViewController.m
+++ windowsystems/iOS/AprilViewController.m
@@ -33,24 +33,82 @@
 	return self;
 }
 
+
+- (CGImageRef)CGImageRotatedByAngle:(CGImageRef)imgRef angle:(CGFloat)angle
+{
+	CGFloat angleInRadians = angle * (M_PI / 180);
+	CGFloat width = CGImageGetWidth(imgRef);
+	CGFloat height = CGImageGetHeight(imgRef);
+	
+	CGRect imgRect = CGRectMake(0, 0, width, height);
+	CGAffineTransform transform = CGAffineTransformMakeRotation(angleInRadians);
+	CGRect rotatedRect = CGRectApplyAffineTransform(imgRect, transform);
+	
+	CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+	CGContextRef bmContext = CGBitmapContextCreate(NULL,
+												   rotatedRect.size.width,
+												   rotatedRect.size.height,
+												   8,
+												   0,
+												   colorSpace,
+												   kCGImageAlphaPremultipliedFirst);
+	CGContextSetAllowsAntialiasing(bmContext, FALSE);
+	CGContextSetInterpolationQuality(bmContext, kCGInterpolationNone);
+	CGColorSpaceRelease(colorSpace);
+	CGContextTranslateCTM(bmContext,
+						  +(rotatedRect.size.width/2),
+						  +(rotatedRect.size.height/2));
+	CGContextRotateCTM(bmContext, angleInRadians);
+	CGContextTranslateCTM(bmContext,
+						  -(rotatedRect.size.width/2),
+						  -(rotatedRect.size.height/2));
+	CGContextDrawImage(bmContext, CGRectMake(0, 0,
+											 rotatedRect.size.width,
+											 rotatedRect.size.height),
+					   imgRef);
+	
+	CGImageRef rotatedImage = CGBitmapContextCreateImage(bmContext);
+	CFRelease(bmContext);
+	[(id)rotatedImage autorelease];
+	
+	return rotatedImage;
+}
+
+
+
 // Implement loadView to create a view hierarchy programmatically, without using a nib.
 - (void)loadView {	
 	[super loadView];
-	NSLog(@"%s", __PRETTY_FUNCTION__);
-
+	
+	UIUserInterfaceIdiom idiom = UIUserInterfaceIdiomPhone;
+	if ([[UIDevice currentDevice] respondsToSelector:@selector(userInterfaceIdiom:)]) 
+	{
+		idiom = [UIDevice currentDevice].userInterfaceIdiom;
+	}
+	else if([UIScreen mainScreen].bounds.size.height == 1024)
+	{
+		idiom = UIUserInterfaceIdiomPad;
+	}
 	
 	NSString *defaultPngName = @"Default";
-	if([UIDevice currentDevice].userInterfaceIdiom == UIUserInterfaceIdiomPad)
+	if(idiom == UIUserInterfaceIdiomPad)
 	{
 		defaultPngName = @"Default-Landscape";
 	}
 	
 	UIImage *image = [UIImage imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:defaultPngName ofType:@"png"] ];
 	;
-	if([UIDevice currentDevice].userInterfaceIdiom == UIUserInterfaceIdiomPhone)
+	if(idiom == UIUserInterfaceIdiomPhone)
 	{
 		// default.png is incorrectly rotated on iphone
-		image = [UIImage imageWithCGImage:image.CGImage scale:1 orientation:UIImageOrientationLeft];
+		// this sadly doesnt work on <4.0:
+		//image = [UIImage imageWithCGImage:image.CGImage scale:1 orientation:UIImageOrientationLeft];
+		
+		// hence we added rotation implementation from http://blog.coriolis.ch/2009/09/04/arbitrary-rotation-of-a-cgimage/
+		// and we use it here:
+		CGImageRef ir = [self CGImageRotatedByAngle:image.CGImage angle:90];
+		image = [UIImage imageWithCGImage:ir];
+		
 	}
 	UIImageView *iv = [[[[UIImageView alloc] initWithImage:image] autorelease] retain];
 
