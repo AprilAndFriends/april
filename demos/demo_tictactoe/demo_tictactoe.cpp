5b806bd25df83e2d39a71fa74592bdc39f2b8f8b
Boris Mikic <boris.blizzard@gmail.com>
2011-02-10 13:04:28 +0000

    - updated demo projects so they can be compiled and run - added VS2008 project for demo_tictactoe - added VS2008 project for mesh_loader_demo

--- demos/demo_tictactoe/demo_tictactoe.cpp
+++ demos/demo_tictactoe/demo_tictactoe.cpp
@@ -8,104 +8,56 @@ Copyright (c) 2010 Kresimir Spes                                                
 * the terms of the BSD license: http://www.opensource.org/licenses/bsd-license.php   *
 \************************************************************************************/
 #include <stdio.h>
-#include "april/RenderSystem.h"
+
+#include <april/RenderSystem.h>
+#include <april/Window.h>
+#include <gtypes/Rectangle.h>
+#include <hltypes/hstring.h>
 
 april::Texture* background;
 april::Texture* x_symbol;
 april::Texture* o_symbol;
-april::Texture* line_hor;
+april::Texture* line_horz;
 april::Texture* line_vert;
 april::Texture* line45;
 april::Texture* line315;
-int positions[3][3] = {{0,0,0},{0,0,0},{0,0,0}};
+grect drawRect(0.0f, 0.0f, 800.0f, 600.0f);
+int positions[3][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
 int victory = 0;
 bool player = 0;
+april::TexturedVertex v[4];
 
-
-
-void draw_symbol(int x, int y, std::string symbol)
+void draw_symbol(int x, int y, chstr symbol)
 {
 	int x1, x2, x3, x4, y1, y2, y3, y4;
 	
-	x1 = x*250 - 250 + (x-1)*25;
-	x2 = x*250 + (x-1)*25;
-	x3 = x*250 - 250 + (x-1)*25;
-	x4 = x*250 + (x-1)*25;
-	y1 = y*185 - 185 + (y-1)*25;
-	y2 = y*185 - 185 + (y-1)*25;
-	y3 = y*185 + (y-1)*25;
-	y4 = y*185 + (y-1)*25;
+	x1 = x * 250 - 250 + (x - 1) * 25;
+	x2 = x * 250 + (x - 1) * 25;
+	x3 = x * 250 - 250 + (x - 1) * 25;
+	x4 = x * 250 + (x - 1) * 25;
+	y1 = y * 185 - 185 + (y - 1) * 25;
+	y2 = y * 185 - 185 + (y - 1) * 25;
+	y3 = y * 185 + (y - 1) * 25;
+	y4 = y * 185 + (y - 1) * 25;
 	
-	/*switch (x)
+	if (symbol == "x_symbol")
 	{
-		case 1:
-			x1 = 0;
-			x2 = 250;
-			x3 = 0;
-			x4 = 250;
-		break;
-		
-		case 2:
-			x1 = 275;
-			x2 = 525;
-			x3 = 275;
-			x4 = 525;
-		break;
-		
-		case 3:
-			x1 = 550;
-			x2 = 800;
-			x3 = 550;
-			x4 = 800;
-		break;
-		
-		default:
-			printf("Wrong parameter 'x'!");
-		break;
+		april::rendersys->setTexture(x_symbol);
+	}
+	else if (symbol == "o_symbol")
+	{
+		april::rendersys->setTexture(o_symbol);
+	}
+	else
+	{
+		printf("Wrong parameter 'symbol'!");
 	}
 	
-	switch (y)
-	{
-		case 1:
-			y1 = 0;
-			y2 = 0;
-			y3 = 185;
-			y4 = 185;
-		break;
-		
-		case 2:
-			y1 = 210;
-			y2 = 210;
-			y3 = 390;
-			y4 = 390;
-		break;
-		
-		case 3:
-			y1 = 415;
-			y2 = 415;
-			y3 = 600;
-			y4 = 600;
-		break;
-		
-		default:
-			printf("Wrong parameter 'y'!");
-		break;
-	}*/
-	
-	if(symbol == "x_symbol")
-		rendersys->setTexture(x_symbol);
-	else if(symbol == "o_symbol")
-		rendersys->setTexture(o_symbol);
-	else
-		printf("Wrong parameter 'symbol'!");
-
-	
-	april::TexturedVertex v[4];
-	v[0].x=x1; v[0].y=y1; v[0].z=0; v[0].u=0; v[0].v=0;
-	v[1].x=x2; v[1].y=y2; v[1].z=0; v[1].u=1; v[1].v=0;
-	v[2].x=x3; v[2].y=y3; v[2].z=0; v[2].u=0; v[2].v=1;
-	v[3].x=x4; v[3].y=y4; v[3].z=0; v[3].u=1; v[3].v=1;
-	rendersys->render(april::TriangleStrip,v,4);
+	v[0].x = x1; v[0].y = y1; v[0].z = 0; v[0].u = 0; v[0].v = 0;
+	v[1].x = x2; v[1].y = y2; v[1].z = 0; v[1].u = 1; v[1].v = 0;
+	v[2].x = x3; v[2].y = y3; v[2].z = 0; v[2].u = 0; v[2].v = 1;
+	v[3].x = x4; v[3].y = y4; v[3].z = 0; v[3].u = 1; v[3].v = 1;
+	april::rendersys->render(april::TriangleStrip, v, 4);
 }
 
 
@@ -113,117 +65,106 @@ void draw_line(int x_start, int y_start, int x_end, int y_end, std::string symbo
 {
 	int x1, x2, x3, x4, y1, y2, y3, y4;
 	
-	x1 = x_start*250 + (x_start+1)*25;
-	x2 = x_end*250 + 250 + (x_start+1)*25;
-	x3 = x_start*250 + (x_start+1)*25;
-	x4 = x_end*250 + 250 + (x_start-1)*25;
-	y1 = y_start*185 + (y_start+1)*25;
-	y2 = y_start*185 + (y_start+1)*25;
-	y3 = y_end*185 + 185 + (y_start+1)*25;
-	y4 = y_end*185 + 185 + (y_start+1)*25;
+	x1 = x_start * 250 + (x_start + 1) * 25;
+	x2 = x_end * 250 + 250 + (x_start - 1) * 25;
+	x3 = x_start * 250 + (x_start + 1) * 25;
+	x4 = x_end * 250 + 250 + (x_start - 1) * 25;
+	y1 = y_start * 185 + (y_start + 1) * 25;
+	y2 = y_start * 185 + (y_start + 1) * 25;
+	y3 = y_end * 185 + 185 + (y_start - 1) * 25;
+	y4 = y_end * 185 + 185 + (y_start - 1) * 25;
 	
-	
-	if(symbol == "line_hor")
-		rendersys->setTexture(line_hor);
-	else if(symbol == "line_vert")
-		rendersys->setTexture(line_vert);
-	else if(symbol == "line45")
-		rendersys->setTexture(line45);
-	else if(symbol == "line315")
-		rendersys->setTexture(line315);
+	if (symbol == "line_horz")
+	{
+		april::rendersys->setTexture(line_horz);
+	}
+	else if (symbol == "line_vert")
+	{
+		april::rendersys->setTexture(line_vert);
+	}
+	else if (symbol == "line45")
+	{
+		april::rendersys->setTexture(line45);
+	}
+	else if (symbol == "line315")
+	{
+		april::rendersys->setTexture(line315);
+	}
 	else
+	{
 		printf("Wrong parameter 'symbol'!");
-
+	}
 	
-	april::TexturedVertex v[4];
-	v[0].x=x1; v[0].y=y1; v[0].z=0; v[0].u=0; v[0].v=0;
-	v[1].x=x2; v[1].y=y2; v[1].z=0; v[1].u=1; v[1].v=0;
-	v[2].x=x3; v[2].y=y3; v[2].z=0; v[2].u=0; v[2].v=1;
-	v[3].x=x4; v[3].y=y4; v[3].z=0; v[3].u=1; v[3].v=1;
-	rendersys->render(april::TriangleStrip,v,4);
+	v[0].x = x1; v[0].y = y1; v[0].z = 0; v[0].u = 0; v[0].v = 0;
+	v[1].x = x2; v[1].y = y2; v[1].z = 0; v[1].u = 1; v[1].v = 0;
+	v[2].x = x3; v[2].y = y3; v[2].z = 0; v[2].u = 0; v[2].v = 1;
+	v[3].x = x4; v[3].y = y4; v[3].z = 0; v[3].u = 1; v[3].v = 1;
+	april::rendersys->render(april::TriangleStrip, v, 4);
 }
 
-
-
-bool render(float time_increase)
+bool update(float k)
 {	
-	rendersys->setViewport(800,600);
+	april::rendersys->clear();
+	april::rendersys->setOrthoProjection(drawRect);
 	
-	rendersys->setTexture(background);
-	april::TexturedVertex v[4];
-	v[0].x=0;   v[0].y=0;   v[0].z=0; v[0].u=0; v[0].v=0;
-	v[1].x=800; v[1].y=0;   v[1].z=0; v[1].u=1; v[1].v=0;
-	v[2].x=0;   v[2].y=600; v[2].z=0; v[2].u=0; v[2].v=1;
-	v[3].x=800; v[3].y=600; v[3].z=0; v[3].u=1; v[3].v=1;
-	rendersys->render(april::TriangleStrip,v,4);
+	april::rendersys->setTexture(background);
+	v[0].x = 0;   v[0].y = 0;   v[0].z = 0; v[0].u = 0; v[0].v = 0;
+	v[1].x = 800; v[1].y = 0;   v[1].z = 0; v[1].u = 1; v[1].v = 0;
+	v[2].x = 0;   v[2].y = 600; v[2].z = 0; v[2].u = 0; v[2].v = 1;
+	v[3].x = 800; v[3].y = 600; v[3].z = 0; v[3].u = 1; v[3].v = 1;
+	april::rendersys->render(april::TriangleStrip, v, 4);
 	
-	rendersys->setTexture(0);
-	rendersys->drawColoredQuad(250,0,25,600,1,1,0,1);
-	rendersys->drawColoredQuad(525,0,25,600,1,1,0,1);
-	rendersys->drawColoredQuad(0,185,800,25,1,1,0,1);
-	rendersys->drawColoredQuad(0,390,800,25,1,1,0,1);
+	april::rendersys->setTexture(0);
+	april::rendersys->drawColoredQuad(grect(250, 0, 25, 600), april::Color::MANGENTA);
+	april::rendersys->drawColoredQuad(grect(525, 0, 25, 600), april::Color::MANGENTA);
+	april::rendersys->drawColoredQuad(grect(0, 185, 800, 25), april::Color::MANGENTA);
+	april::rendersys->drawColoredQuad(grect(0, 390, 800, 25), april::Color::MANGENTA);
 	
+	for (int j = 0; j < 3; j++)
+	{
+		for (int i = 0; i < 3; i++)
+		{
+			if (positions[i][j] == 1)
+			{
+				draw_symbol(i + 1, j + 1, "x_symbol");
+			}
+			else if (positions[i][j] == 2)
+			{
+				draw_symbol(i + 1, j + 1, "o_symbol");
+			}
+		}
+	}
 		
-	if (positions[0][0] == 1)
-		draw_symbol(1,1,"x_symbol");
-	if (positions[0][1] == 1)
-		draw_symbol(1,2,"x_symbol");
-	if (positions[0][2] == 1)
-		draw_symbol(1,3,"x_symbol");
-	if (positions[1][0] == 1)
-		draw_symbol(2,1,"x_symbol");
-	if (positions[1][1] == 1)
-		draw_symbol(2,2,"x_symbol");
-	if (positions[1][2] == 1)
-		draw_symbol(2,3,"x_symbol");
-	if (positions[2][0] == 1)
-		draw_symbol(3,1,"x_symbol");
-	if (positions[2][1] == 1)
-		draw_symbol(3,2,"x_symbol");
-	if (positions[2][2] == 1)
-		draw_symbol(3,3,"x_symbol");
-		
-	if (positions[0][0] == 2)
-		draw_symbol(1,1,"o_symbol");
-	if (positions[0][1] == 2)
-		draw_symbol(1,2,"o_symbol");
-	if (positions[0][2] == 2)
-		draw_symbol(1,3,"o_symbol");
-	if (positions[1][0] == 2)
-		draw_symbol(2,1,"o_symbol");
-	if (positions[1][1] == 2)
-		draw_symbol(2,2,"o_symbol");
-	if (positions[1][2] == 2)
-		draw_symbol(2,3,"o_symbol");
-	if (positions[2][0] == 2)
-		draw_symbol(3,1,"o_symbol");
-	if (positions[2][1] == 2)
-		draw_symbol(3,2,"o_symbol");
-	if (positions[2][2] == 2)
-		draw_symbol(3,3,"o_symbol");
-		
-		
-	if (victory == 1)
-		draw_line(0,0,0,2,"line_hor");
-	if (victory == 2)
-		draw_line(1,0,1,2,"line_hor");
-	if (victory == 3)
-		draw_line(2,0,2,2,"line_hor");
-	if (victory == 4)
-		draw_line(0,0,2,0,"line_vert");
-	if (victory == 5)
-		draw_line(0,1,2,1,"line_vert");
-	if (victory == 6)
-		draw_line(0,2,2,2,"line_vert");
-	if (victory == 7)
-		draw_line(0,0,2,2,"line45");
-	if (victory == 8)
-		draw_line(0,0,2,2,"line315");
+	switch (victory)
+	{
+	case 1:
+		draw_line(0, 0, 0, 2, "line_horz");
+		break;
+	case 2:
+		draw_line(1, 0, 1, 2, "line_horz");
+		break;
+	case 3:
+		draw_line(2, 0, 2, 2, "line_horz");
+		break;
+	case 4:
+		draw_line(0, 0, 2, 0, "line_vert");
+		break;
+	case 5:
+		draw_line(0, 1, 2, 1, "line_vert");
+		break;
+	case 6:
+		draw_line(0, 2, 2, 2, "line_vert");
+		break;
+	case 7:
+		draw_line(0, 0, 2, 2, "line45");
+		break;
+	case 8:
+		draw_line(0, 0, 2, 2, "line315");
+		break;
+	}
 	
 	return true;
 }
-
-
 
 void OnMouseUp(float x,float y,int button)
 {
@@ -401,18 +342,17 @@ void OnMouseUp(float x,float y,int button)
 
 int main()
 {
-	april::init("OpenGL",800,600,0,"demo_tictactoe");
-	rendersys->registerUpdateCallback(render);
-	rendersys->registerMouseCallbacks(0,OnMouseUp,0);
-	background=rendersys->loadTexture("../media/texture.jpg");
-	x_symbol=rendersys->loadTexture("../media/x.png");
-	o_symbol=rendersys->loadTexture("../media/o.png");
-	line_hor=rendersys->loadTexture("../media/line_hor.png");
-	line_vert=rendersys->loadTexture("../media/line_vert.png");
-	line45=rendersys->loadTexture("../media/line45.png");
-	line315=rendersys->loadTexture("../media/line315.png");
-
-	rendersys->enterMainLoop();
+	april::init("renderer", drawRect.w, drawRect.h, false, "demo_tictactoe");
+	april::rendersys->getWindow()->setUpdateCallback(update);
+	april::rendersys->getWindow()->setMouseCallbacks(NULL, OnMouseUp, NULL);
+	background = april::rendersys->loadTexture("../media/texture.jpg");
+	x_symbol = april::rendersys->loadTexture("../media/x.png");
+	o_symbol = april::rendersys->loadTexture("../media/o.png");
+	line_horz = april::rendersys->loadTexture("../media/line_horz.png");
+	line_vert = april::rendersys->loadTexture("../media/line_vert.png");
+	line45 = april::rendersys->loadTexture("../media/line45.png");
+	line315 = april::rendersys->loadTexture("../media/line315.png");
+	april::rendersys->getWindow()->enterMainLoop();
 	april::destroy();
 	return 0;
 }
