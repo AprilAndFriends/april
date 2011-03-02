65f552aa6042bccc75fb970bd603ef7496be3348
Boris Mikic <boris.blizzard@gmail.com>
2011-03-02 12:16:10 +0000

    - added more Color constructors

--- include/april/Color.h
+++ include/april/Color.h
@@ -24,16 +24,18 @@ namespace april
 		unsigned char b;
 		unsigned char a;
 		
+		Color();
 		Color(int r, int g, int b, int a = 255);
 		Color(unsigned char r, unsigned char g, unsigned char b, unsigned char a = 255);
 		Color(unsigned int color);
 		Color(chstr hex);
-		Color();
+		Color(Color color, unsigned char a);
 
 		void set(int r, int g, int b, int a = 255);
 		void set(unsigned char r, unsigned char g, unsigned char b, unsigned char a = 255);
 		void set(unsigned int color);
 		void set(chstr hex);
+		void set(Color color, unsigned char a);
 
 		float r_f() { return r / 255.0f; }
 		float g_f() { return g / 255.0f; }
@@ -59,15 +61,6 @@ namespace april
 		Color operator/=(Color& other);
 		Color operator*=(float value);
 		Color operator/=(float value);
-		
-		// DEPRECATED
-		Color(float r, float g, float b, float a = 1.0f) DEPRECATED_ATTRIBUTE;
-		void setColor(float r, float g, float b, float a = 1.0f) DEPRECATED_ATTRIBUTE;
-		void setColor(int r, int g, int b, int a = 255) DEPRECATED_ATTRIBUTE;
-		void setColor(unsigned char r, unsigned char g, unsigned char b, unsigned char a = 255) DEPRECATED_ATTRIBUTE;
-		void setColor(unsigned int color) DEPRECATED_ATTRIBUTE;
-		void setColor(chstr hex) DEPRECATED_ATTRIBUTE;
-		void set(float r, float g, float b, float a = 1.0f) DEPRECATED_ATTRIBUTE;
 		
 		// predefined colors
 		#define RED			Color(255,   0,   0)
