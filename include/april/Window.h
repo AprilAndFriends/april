7cf0e3643b8ef9d5f612446cf7123c45cb8e4e89
Boris Mikic <boris.blizzard@gmail.com>
2011-07-25 07:53:51 +0000

    - added proper deprecation warnings for VS

--- include/april/Window.h
+++ include/april/Window.h
@@ -85,18 +85,18 @@ namespace april
 		virtual bool isSystemCursorShown() = 0;
 		virtual int getWidth() = 0;
 		virtual int getHeight() = 0;
-		int getWindowWidth() DEPRECATED_ATTRIBUTE { return getWidth(); }
-		int getWindowHeight() DEPRECATED_ATTRIBUTE { return getHeight(); }
+		DEPRECATED_ATTRIBUTE int getWindowWidth() { return getWidth(); }
+		DEPRECATED_ATTRIBUTE int getWindowHeight() { return getHeight(); }
 		virtual void setWindowTitle(chstr title) = 0;
 		hstr getWindowTitle() { return mTitle; }
 		virtual gvec2 getCursorPosition() = 0;
 		virtual bool isCursorInside();
-		gvec2 getCursorPos() DEPRECATED_ATTRIBUTE { return getCursorPosition(); }
+		DEPRECATED_ATTRIBUTE gvec2 getCursorPos() { return getCursorPosition(); }
 		virtual void presentFrame() = 0;
 		virtual void* getIDFromBackend() = 0;
 		virtual void doEvents() = 0;
 		float getAspectRatio();
-		float getWindowAspectRatio() DEPRECATED_ATTRIBUTE { return getAspectRatio(); }
+		DEPRECATED_ATTRIBUTE float getWindowAspectRatio() { return getAspectRatio(); }
 		
 		// misc virtuals
 		virtual bool isFullscreen();
--- include/april/aprilExport.h
+++ include/april/aprilExport.h
@@ -28,7 +28,7 @@ Copyright (c) 2010 Kresimir Spes                                                
 	#endif
 	#ifndef DEPRECATED_ATTRIBUTE
 		#ifdef _MSC_VER
-			#define DEPRECATED_ATTRIBUTE
+			#define DEPRECATED_ATTRIBUTE __declspec(deprecated("function is deprecated"))
 		#else
 			#define DEPRECATED_ATTRIBUTE __attribute__((deprecated))
 		#endif
