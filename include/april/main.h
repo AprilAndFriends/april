aaf701eb1342f0c0f95c830ed11296e172e43258
Boris Mikic <boris.blizzard@gmail.com>
2011-12-21 12:00:45 +0000

    - (ivucica) hltypes update for hdir::chdir

--- include/april/main.h
+++ include/april/main.h
@@ -80,8 +80,73 @@ int main (int argc, char **argv)
 #include <windows.h>
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-    april_main(april_init, april_destroy, 0, NULL);
-    return 0;
+	// origin: http://www.flipcode.com/archives/WinMain_Command_Line_Parser.shtml
+	//  WinMain Command Line Parser by Max McGuire
+
+    int    argc;
+    char** argv;
+
+    char*  arg;
+    int    index;
+    int    result;
+
+    // count the arguments
+    
+    argc = 1;
+    arg  = lpCmdLine;
+    
+    while (arg[0] != 0) 
+	{
+
+        while (arg[0] != 0 && arg[0] == ' ')
+		    arg++;
+        
+        if (arg[0] != 0) 
+		{
+            argc++;
+            while (arg[0] != 0 && arg[0] != ' ')
+                arg++;
+        }
+    }    
+    
+    // tokenize the arguments
+    argv = (char**)malloc(argc * sizeof(char*));
+    arg = lpCmdLine;
+    index = 1;
+
+    while (arg[0] != 0) 
+	{
+        while (arg[0] != 0 && arg[0] == ' ') 
+            arg++;
+
+        if (arg[0] != 0) 
+		{
+            argv[index] = arg;
+            index++;
+        
+            while (arg[0] != 0 && arg[0] != ' ') 
+                arg++;
+        
+            if (arg[0] != 0) 
+			{
+                arg[0] = 0;    
+                arg++;
+            }
+        }
+    }    
+
+    // put the program name into argv[0]
+
+    char filename[_MAX_PATH];
+    
+    GetModuleFileName(NULL, filename, _MAX_PATH);
+    argv[0] = filename;
+
+	// call the user specified main function
+    april_main(april_init, april_destroy, argc, argv);
+    
+	free(argv);
+	return 0;
 }
 #endif
 #define main __ STOP_USING_MAIN___DEPRECATED_IN_APRIL
