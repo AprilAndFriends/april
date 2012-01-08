110e35c92334dbf850728b308c8a79d2648d7a3b
Boris Mikic <boris.blizzard@gmail.com>
2012-01-08 21:02:27 +0000

    - added branches/shaders

---
+++ include/april/PixelShader.h
@@ -0,0 +1,40 @@
+/// @file
+/// @author  Boris Mikic
+/// @version 1.32
+/// 
+/// @section LICENSE
+/// 
+/// This program is free software; you can redistribute it and/or modify it under
+/// the terms of the BSD license: http://www.opensource.org/licenses/bsd-license.php
+/// 
+/// @section DESCRIPTION
+/// 
+/// Defines a generic pixel shader.
+
+#ifndef APRIL_PIXEL_SHADER_H
+#define APRIL_PIXEL_SHADER_H
+
+#include <hltypes/hstring.h>
+
+#include "aprilExport.h"
+
+namespace april
+{
+	class aprilExport PixelShader
+	{
+	public:
+		PixelShader();
+		virtual ~PixelShader();
+
+		void load(chstr filename);
+		bool compile();
+		virtual bool compile(chstr shaderCode) = 0;
+
+	protected:
+		hstr mShaderCode;
+
+	};
+
+}
+
+#endif
---
+++ rendersystems/DirectX9/DirectX9_VertexShader.h
@@ -0,0 +1,38 @@
+/// @file
+/// @author  Boris Mikic
+/// @version 1.32
+/// 
+/// @section LICENSE
+/// 
+/// This program is free software; you can redistribute it and/or modify it under
+/// the terms of the BSD license: http://www.opensource.org/licenses/bsd-license.php
+/// 
+/// @section DESCRIPTION
+/// 
+/// Defines a DirectX vertex shader.
+
+#ifdef _DIRECTX9
+#ifndef APRIL_DIRECTX9_VERTEX_SHADER_H
+#define APRIL_DIRECTX9_VERTEX_SHADER_H
+
+#include "VertexShader.h"
+
+struct IDirect3DVertexShader9;
+
+namespace april
+{
+	class DirectX9_VertexShader : public VertexShader
+	{
+	public:
+		IDirect3DVertexShader9* mShader;
+
+		DirectX9_VertexShader();
+		~DirectX9_VertexShader();
+
+		bool compile(chstr shaderCode);
+
+	};
+
+}
+#endif
+#endif
