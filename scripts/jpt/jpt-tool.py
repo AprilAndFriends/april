62cf9274d269435b30f0af769a9c169e6eaa52b1
Boris Mikic <boris.blizzard@gmail.com>
2012-05-11 14:45:52 +0000

    - added PIL support for using custom image formats directly for conversion to JPEG+PNG and merging to JPT

--- scripts/jpt/jpt-tool.py
+++ scripts/jpt/jpt-tool.py
@@ -15,18 +15,38 @@ def process():
 		info()
 	elif sys.argv[1].lower() in ("help", "-h", "/h", "-?", "/?"):
 		help()
-	elif len(sys.argv) != 5:
-		info()
 	elif sys.argv[1].lower() == "merge":
-		print Jpt.merge(sys.argv[2], sys.argv[3], sys.argv[4])
+		if len(sys.argv) != 5:
+			info()
+		else:
+			print Jpt.merge(sys.argv[2], sys.argv[3], sys.argv[4])
 	elif sys.argv[1].lower() == "split":
-		print Jpt.split(sys.argv[2], sys.argv[3], sys.argv[4])
+		if len(sys.argv) != 5:
+			info()
+		else:
+			print Jpt.split(sys.argv[2], sys.argv[3], sys.argv[4])
+	elif sys.argv[1].lower() == "convert":
+		try:
+			import Image
+			from pilconv import PilConv
+		except:
+			print "ERROR! Please install PIL to use the 'convert' command."
+			print "http://www.pythonware.com/products/pil"
+			return
+		if len(sys.argv) != 5 and len(sys.argv) != 6:
+			info()
+		else:
+			quality = 75
+			if len(sys.argv) == 6:
+				quality = int(sys.argv[5])
+			print PilConv.convert(sys.argv[2], sys.argv[3], sys.argv[4], quality)
 	else:
 		info()
 
 def info():
 	print ""
 	print "usage: jpt-tool.py merge|split JPT_FILENAME JPEG_FILENAME PNG_FILNAME"
+	print "       jpt-tool.py convert FILENAME JPEG_FILENAME PNG_FILNAME [JPEG_QUALITY]"
 	print "       use 'jpt-tool.py -h' for more information"
 	print ""
 	os.system("pause")
@@ -34,10 +54,16 @@ def info():
 def help():
 	print ""
 	print "usage: jpt-tool.py merge|split JPT_FILENAME JPEG_FILENAME PNG_FILNAME"
+	print "       jpt-tool.py convert FILENAME JPEG_FILENAME PNG_FILNAME JPEG_QUALITY"
 	print ""
 	print "commands:"
-	print "merge      - merges a JPEG and a PNG file into a JPT file"
-	print "split      - splits a JPT file to a JPEG and a PNG file"
+	print "merge            - merges a JPEG and a PNG file into a JPT file"
+	print "split            - splits a JPT file to a JPEG and a PNG file"
+	print "JPT_FILENAME     - JPT filename to use in the process"
+	print "JPEG_FILENAME    - JPEG filename to use in the process"
+	print "PNG_FILENAME     - PNG filename to use in the process"
+	print "FILENAME         - image filename to use in the process"
+	print "JPEG_QUALITY     - value from 1 (worst) to 95 (best) for JPEG compression quality (default = 75); values above 95 should be avoided, 100 completely disables the quantization stage"
 	print ""
 	os.system("pause")
 
