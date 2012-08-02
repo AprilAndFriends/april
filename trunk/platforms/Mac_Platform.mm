c574ee69033301fc0bc2254370e08e5ea8e69513
kspes <kspes@a048fc06-0db0-1d88-0edd-9057f1fe799b>
2012-08-02 19:06:07 +0000

    small api fix in mac platform call

--- trunk/platforms/Mac_Platform.mm
+++ trunk/platforms/Mac_Platform.mm
@@ -173,9 +173,9 @@ namespace april
 		}
 		
 		NSString *titlens = [NSString stringWithUTF8String:title.c_str()];
-		NSString *textns = [NSString stringWithUTF8String:text.c_str()];
+//		NSString *textns = [NSString stringWithUTF8String:text.c_str()];
 		
-		int clicked = NSRunAlertPanel(titlens, textns, buttons[0], buttons[1], buttons[2]);
+		int clicked = NSRunAlertPanel(titlens, @"%s", buttons[0], buttons[1], buttons[2], text.c_str());
 		switch (clicked)
 		{
 		case NSAlertDefaultReturn:
