47adcb81d6ff75fbca40791ae58d20174ca5d88a
Boris Mikic <boris.blizzard@gmail.com>
2012-09-05 06:50:19 +0000

    - improved how the april::getDisplayResolution implementation works on Android

--- android/src/net/sourceforge/april/android/Renderer.java
+++ android/src/net/sourceforge/april/android/Renderer.java
@@ -1,13 +1,11 @@
 package net.sourceforge.april.android;
 
-// version 2.1
+// version 2.14
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 import android.app.Activity;
-import android.os.Build;
-import android.util.DisplayMetrics;
 
 public class Renderer implements android.opengl.GLSurfaceView.Renderer
 {
@@ -16,23 +14,10 @@ public class Renderer implements android.opengl.GLSurfaceView.Renderer
 		NativeInterface.onSurfaceCreated();
 		if (!NativeInterface.Running)
 		{
-			NativeInterface.setVariables(NativeInterface.Activity, NativeInterface.SystemPath, NativeInterface.DataPath, NativeInterface.PackageName, NativeInterface.VersionCode, NativeInterface.ArchivePath);
-			DisplayMetrics metrics = new DisplayMetrics();
-			NativeInterface.Activity.getWindowManager().getDefaultDisplay().getMetrics(metrics);
+			NativeInterface.setVariables(NativeInterface.Activity, NativeInterface.SystemPath, NativeInterface.DataPath,
+				NativeInterface.PackageName, NativeInterface.VersionCode, NativeInterface.ArchivePath);
 			String args[] = {NativeInterface.ArchivePath}; // adding argv[0]
-			int width = metrics.widthPixels;
-			int height = metrics.heightPixels;
-			if (height > width)
-			{
-				height = metrics.widthPixels;
-				width = metrics.heightPixels;
-			}
-			// fixes problem with bottom 20 pixels being covered by Kindle Fire's menu
-			if (Build.MANUFACTURER == "Amazon" && Build.MODEL == "Kindle Fire")
-			{
-				height -= 20;
-			}
-			NativeInterface.init(args, width, height);
+			NativeInterface.init(args);
 			NativeInterface.Running = true;
 		}
 	}
