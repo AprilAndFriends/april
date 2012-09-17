7cf15f5b37231ce0b2363ed17a75230f3bb25c30
kspes <kspes@a048fc06-0db0-1d88-0edd-9057f1fe799b>
2012-09-17 13:26:44 +0000

    added dpi settings to iOS platform

--- trunk/platforms/iOS_Platform.mm
+++ trunk/platforms/iOS_Platform.mm
@@ -143,16 +143,25 @@ namespace april
 				{
 					info.name = "iPad1";
 					info.ram = 256;
+					info.displayDpi = 132;
 				}
 				else if (name.starts_with("iPad2"))
 				{
 					info.name = "iPad2";
 					info.ram = 512;
+					info.displayDpi = 132;
 				}
 				else if (name.starts_with("iPad3"))
 				{
 					info.name = "iPad3";
 					info.ram = 1024;
+					info.displayDpi = 264;
+				}
+				else
+				{
+					info.name = "iPad?";
+					info.ram = 1024;
+					info.displayDpi = 264;
 				}
 			}
 			else if (name.starts_with("iPhone"))
@@ -161,31 +170,43 @@ namespace april
 				{
 					info.name = "iPhone2G";
 					info.ram = 128;
+					info.displayDpi = 163;
 				}
 				else if (name == "iPhone1,2")
 				{
 					info.name = "iPhone3G";
 					info.ram = 128;
+					info.displayDpi = 163;
 				}
 				else if (name == "iPhone2,1")
 				{
 					info.name = "iPhone3GS";
 					info.ram = 256;
+					info.displayDpi = 163;
 				}
 				else if (name.starts_with("iPhone3"))
 				{
 					info.name = "iPhone4";
 					info.ram = 512;
+					info.displayDpi = 326;
 				}
 				else if (name.starts_with("iPhone4"))
 				{
 					info.name = "iPhone4S";
 					info.ram = 512;
+					info.displayDpi = 326;
 				}
 				else if (name.starts_with("iPhone5"))
 				{
 					info.name = "iPhone5";
 					info.ram = 1024;
+					info.displayDpi = 326;
+				}
+				else
+				{
+					info.name = "iPhone?";
+					info.ram = 1024;
+					info.displayDpi = 326;
 				}
 			}
 			else if (name.starts_with("iPod"))
@@ -194,21 +215,31 @@ namespace april
 				{
 					info.name = "iPod1";
 					info.ram = 128;
+					info.displayDpi = 163;
 				}
 				else if (name == "iPod2,1")
 				{
 					info.name = "iPod2";
 					info.ram = 128;
+					info.displayDpi = 163;
 				}
 				else if (name == "iPod3,1")
 				{
 					info.name = "iPod3";
 					info.ram = 256;
+					info.displayDpi = 163;
 				}
 				else if (name == "iPod4,1")
 				{
 					info.name = "iPod4";
 					info.ram = 256;
+					info.displayDpi = 326;
+				}
+				else
+				{
+					info.name = "iPod?";
+					info.ram = 512;
+					info.displayDpi = 326;
 				}
 			}
 			//else: i386 (iphone simulator) and possible future device types
