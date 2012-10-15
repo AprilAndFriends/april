9f72fb23c8b96efc3e4df7d491810cf988d5b230
Boris Mikic <boris.blizzard@gmail.com>
2012-10-15 13:45:44 +0000

    - added files for WinRT window systems

---
+++ windowsystems/WinRT/WinRT_Window.cpp
@@ -0,0 +1,15 @@
+/// @file
+/// @author  Boris Mikic
+/// @version 2.41
+/// 
+/// @section LICENSE
+/// 
+/// This program is free software; you can redistribute it and/or modify it under
+/// the terms of the BSD license: http://www.opensource.org/licenses/bsd-license.php
+
+#ifdef _WIN32
+#include <hltypes/hplatform.h>
+#if _HL_WINRT
+
+#endif
+#endif
---
+++ windowsystems/WinRT/WinRT_Window.h
@@ -0,0 +1,22 @@
+/// @file
+/// @author  Boris Mikic
+/// @version 2.41
+/// 
+/// @section LICENSE
+/// 
+/// This program is free software; you can redistribute it and/or modify it under
+/// the terms of the BSD license: http://www.opensource.org/licenses/bsd-license.php
+/// 
+/// @section DESCRIPTION
+/// 
+/// Defines a WinRT window.
+
+#ifdef _WIN32
+#ifndef APRIL_WINRT_WINDOW_H
+#define APRIL_WINRT_WINDOW_H
+#include <hltypes/hplatform.h>
+#if _HL_WINRT
+
+#endif
+#endif
+#endif
