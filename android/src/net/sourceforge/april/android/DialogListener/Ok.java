57e034120ad1bb0774fc5e2f8f09282ef2fd1546
Boris Mikic <boris.blizzard@gmail.com>
2013-01-21 12:40:50 +0000

    - merged all revisions from trunk

--- android/src/net/sourceforge/april/android/DialogListener/Ok.java
+++ android/src/net/sourceforge/april/android/DialogListener/Ok.java
@@ -1,6 +1,6 @@
 package net.sourceforge.april.android.DialogListener;
 
-// version 2.5
+// version 2.53
 
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
@@ -10,6 +10,13 @@ public class Ok implements OnClickListener
 {
 	public void onClick(DialogInterface dialog, int id)
 	{
-		NativeInterface.onDialogOk();
+		NativeInterface.Activity.GlView.queueEvent(new Runnable()
+		{
+			public void run()
+			{
+				NativeInterface.onDialogOk();
+			}
+		});
 	}
+	
 }
