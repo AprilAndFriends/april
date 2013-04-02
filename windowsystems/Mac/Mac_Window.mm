8b4741eb8a1152c32cad4863d8820dddd4af9fd6
kspes <kresimir.spes@gmail.com>
2013-04-02 14:19:07 +0000

    started Mac Cocoa window implementation

---
+++ windowsystems/Mac/Mac_Window.mm
@@ -0,0 +1,22 @@
+/// @file
+/// @author  Kresimir Spes
+/// @version 3.0
+///
+/// @section LICENSE
+///
+/// This program is free software; you can redistribute it and/or modify it under
+/// the terms of the BSD license: http://www.opensource.org/licenses/bsd-license.php
+#include "Mac_Window.h"
+
+namespace april
+{
+    Mac_Window::Mac_Window()
+    {
+
+    }
+
+    Mac_Window::~Mac_Window()
+    {
+        
+    }
+}
