c7296256ae51cc49fab27200fac987deb5260e15
kspes <kresimir.spes@gmail.com>
2013-04-05 14:14:24 +0000

    * mac window input, first methods * replaced window delegate and turned it all into a subclassed cocoa window

--- windowsystems/Mac/Mac_Window.h
+++ windowsystems/Mac/Mac_Window.h
@@ -38,6 +38,8 @@ namespace april
 		hstr getParam(chstr param);
 		void setParam(chstr param, chstr value);
 		
+		void updateCursorPosition(gvec2& pos);
+		
         void presentFrame();
 		bool updateOneFrame();
 	protected:
