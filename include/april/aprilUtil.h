33272ad9f53ca336fea8e6acb7d3d9d423af6de3
Boris Mikic <boris.blizzard@gmail.com>
2014-02-04 14:13:16 +0000

    - added more deprecated values

--- include/april/aprilUtil.h
+++ include/april/aprilUtil.h
@@ -51,6 +51,25 @@ namespace april
 		CM_UNDEFINED = 0x7FFFFFFF
 	};
 
+	DEPRECATED_ATTRIBUTE extern RenderOperation TriangleList;
+	DEPRECATED_ATTRIBUTE extern RenderOperation TriangleStrip;
+	DEPRECATED_ATTRIBUTE extern RenderOperation TriangleFan;
+	DEPRECATED_ATTRIBUTE extern RenderOperation LineList;
+	DEPRECATED_ATTRIBUTE extern RenderOperation LineStrip;
+	DEPRECATED_ATTRIBUTE extern RenderOperation PointList;
+	DEPRECATED_ATTRIBUTE extern RenderOperation RENDER_OP_UNDEFINED;
+	DEPRECATED_ATTRIBUTE extern BlendMode DEFAULT;
+	DEPRECATED_ATTRIBUTE extern BlendMode ALPHA_BLEND;
+	DEPRECATED_ATTRIBUTE extern BlendMode ADD;
+	DEPRECATED_ATTRIBUTE extern BlendMode SUBTRACT;
+	DEPRECATED_ATTRIBUTE extern BlendMode OVERWRITE;
+	DEPRECATED_ATTRIBUTE extern BlendMode BLEND_MODE_UNDEFINED;
+	DEPRECATED_ATTRIBUTE extern ColorMode NORMAL;
+	DEPRECATED_ATTRIBUTE extern ColorMode MULTIPLY;
+	DEPRECATED_ATTRIBUTE extern ColorMode LERP;
+	DEPRECATED_ATTRIBUTE extern ColorMode ALPHA_MAP;
+	DEPRECATED_ATTRIBUTE extern ColorMode COLOR_MODE_UNDEFINED;
+	
 	struct aprilExport PlainVertex : public gvec3
 	{
 	public:
