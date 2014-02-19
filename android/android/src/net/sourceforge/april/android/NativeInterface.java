71527199e70c3462967e5264e8388833911caa38
Boris Mikic <boris.blizzard@gmail.com>
2014-02-19 07:22:30 +0000

    - merged from trunk

--- android/android/src/net/sourceforge/april/android/NativeInterface.java
+++ android/android/src/net/sourceforge/april/android/NativeInterface.java
@@ -106,7 +106,7 @@ public class NativeInterface
 			width = metrics.heightPixels;
 		}
 		// fixes problem with bottom 20 pixels being covered by Kindle Fire's menu
-		if (Build.MANUFACTURER == "Amazon" && Build.MODEL == "Kindle Fire")
+		if (Build.MANUFACTURER.equals("Amazon") && Build.MODEL.equals("Kindle Fire"))
 		{
 			height -= 20;
 		}
@@ -116,6 +116,12 @@ public class NativeInterface
 	
 	public static int getDisplayDpi()
 	{
+		// hardcoded exceptions for known devices that return wrong DPI
+		if (Build.MANUFACTURER.equals("HTC"))
+		{
+			if (Build.MODEL.equals("HTC One X")) return 312;
+		}
+
 		DisplayMetrics metrics = new DisplayMetrics();
 		NativeInterface.Activity.getWindowManager().getDefaultDisplay().getMetrics(metrics);
 		return (int)Math.sqrt((metrics.xdpi * metrics.xdpi + metrics.ydpi * metrics.ydpi) / 2.0);
