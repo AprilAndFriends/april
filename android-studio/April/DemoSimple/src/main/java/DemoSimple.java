a29c2e5afcdf7d042958995f3dcac789d3e9404b
Boris Mikic <boris.blizzard@gmail.com>
2014-04-28 12:41:25 +0000

    - Android Studio update

---
+++ android-studio/April/DemoSimple/src/main/java/DemoSimple.java
@@ -0,0 +1,17 @@
+package com.googlecode.april.android.demoSimple;
+
+public class DemoSimple extends com.googlecode.april.android.Activity
+{
+	static
+	{
+		System.loadLibrary("demo_simple");
+	}
+	
+	@Override
+	protected void onCreate(android.os.Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		this.forceArchivePath(com.googlecode.april.android.NativeInterface.ApkPath); // forces APK as archive file
+	}
+	
+}
