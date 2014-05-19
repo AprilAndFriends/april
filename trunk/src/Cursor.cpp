2aca40f4e5f7d4488845c1b0365006249af79fc3
kresimir.spes <kresimir.spes@a048fc06-0db0-1d88-0edd-9057f1fe799b>
2014-05-19 13:17:47 +0000

    prepared files for cursor class implementation

---
+++ trunk/src/Cursor.cpp
@@ -0,0 +1,10 @@
+/// @file
+/// @author  Kresimir Spes
+/// @author  Boris Mikic
+/// @version 3.3
+///
+/// @section LICENSE
+///
+/// This program is free software; you can redistribute it and/or modify it under
+/// the terms of the BSD license: http://opensource.org/licenses/BSD-3-Clause
+
