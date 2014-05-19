bd84ebaf72842b00f2da5800b0d26776c4709bd3
kresimir.spes <kresimir.spes@gmail.com>
2014-05-19 13:17:47 +0000

    prepared files for cursor class implementation

---
+++ windowsystems/Mac/Mac_Cursor.cpp
@@ -0,0 +1,8 @@
+/// @file
+/// @author  Kresimir Spes
+/// @version 3.3
+///
+/// @section LICENSE
+///
+/// This program is free software; you can redistribute it and/or modify it under
+/// the terms of the BSD license: http://opensource.org/licenses/BSD-3-Clause
