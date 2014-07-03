7dfba43908583d6c3cc6b6ebad404ebd6a563e0c
boris.blizzard <boris.blizzard@a048fc06-0db0-1d88-0edd-9057f1fe799b>
2014-07-03 06:51:33 +0000

    - hopefully last WinRT high DPI device fixes (said every programmer ever)

--- trunk/src/windowsystems/WinRT/WinRT_BaseApp.cpp
+++ trunk/src/windowsystems/WinRT/WinRT_BaseApp.cpp
@@ -370,8 +370,9 @@ namespace april
 		static int screenHeight = 0;
 		april::SystemInfo info = april::getSystemInfo();
 		// WinRT is dumb
-		x *= info.displayDpi / 96.0f;
-		y *= info.displayDpi / 96.0f;
+		float dpiRatio = (info.displayDpi / 96.0f);
+		x *= dpiRatio;
+		y *= dpiRatio;
 		if (screenWidth == 0 || screenHeight == 0)
 		{
 			screenWidth = hround(info.displayResolution.x);
@@ -383,11 +384,11 @@ namespace april
 #ifndef _WINP8
 		if (ApplicationView::Value == ApplicationViewState::Filled)
 		{
-			w -= WINRT_SNAPPED_VIEW_UNUSED;
+			w -= (int)(WINRT_SNAPPED_VIEW_UNUSED * dpiRatio);
 		}
 		else if (ApplicationView::Value == ApplicationViewState::Snapped)
 		{
-			w = WINRT_SNAPPED_VIEW_UNUSED;
+			w = (int)(WINRT_SNAPPED_VIEW_UNUSED * dpiRatio);
 		}
 #else
 		int rotation = WinRT::getScreenRotation();
