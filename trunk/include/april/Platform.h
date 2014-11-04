c1f7d65f4cf9d54a4a25162ec34f6cb7a3d1b81a
boris.blizzard <boris.blizzard@a048fc06-0db0-1d88-0edd-9057f1fe799b>
2014-11-04 11:01:28 +0000

    - continued docs

--- trunk/include/april/Platform.h
+++ trunk/include/april/Platform.h
@@ -21,74 +21,111 @@
 
 namespace april
 {
+	/// @brief Provides system information.
 	struct aprilExport SystemInfo
 	{
+		/// @brief Name of the OS.
 		hstr name;
+		/// @brief CPU architecture.
 		hstr architecture;
+		/// @brief On how many bits the architecture operates (e.g. 32 bit or 64 bit).
 		int architectureBits;
+		/// @brief Version of the OS.
 		float osVersion;
-		int cpuCores; // number of CPU cores or separate CPU units
-		int ram; // how many MB of RAM does the host system have in total
+		/// @brief Number of logical CPU cores (NOT physical!).
+		int cpuCores;
+		/// @brief How many MB of RAM the system has installed.
+		/// @note On some platforms this information is not available directly and might return some values that are note entirely accurate.
+		int ram;
+		/// @brief Current screen resolution.
 		gvec2 displayResolution;
+		/// @brief Current screen DPI.
 		float displayDpi;
-		hstr locale; // locale code as per ISO 639
-		hstr localeVariant; // locale script and region codes as per ISO 15924 (for scripts) and ISO 3166 (for regions)
+		/// @brief Locale code as per ISO 639
+		hstr locale;
+		/// @brief Locale script and region codes as per ISO 15924 (for scripts) and ISO 3166 (for regions).
+		/// @note If available, the "script" (ISO 15924) will always be first and the region (ISO 3166) will always be second. In that case they are separated by a "-" (dash) character.
+		hstr localeVariant;
 
+		/// @note Basic constructor.
 		SystemInfo();
+		/// @note Destructor.
 		~SystemInfo();
 		
 	};
 	
+	/// @brief Defines possible button combinations in a message box.
 	enum MessageBoxButton
 	{
+		/// @brief Just an "OK" button.
 		MESSAGE_BUTTON_OK = 1,
+		/// @brief Just a "CANCEL" button.
 		MESSAGE_BUTTON_CANCEL = 2,
+		/// @brief Just a "YES" button.
 		MESSAGE_BUTTON_YES = 4,
+		/// @brief Just a "NO" button.
 		MESSAGE_BUTTON_NO = 8,
+		/// @brief An "OK" button with a "CANCEL" button.
 		MESSAGE_BUTTON_OK_CANCEL = MESSAGE_BUTTON_OK | MESSAGE_BUTTON_CANCEL,
+		/// @brief A "YES" button with a "NO" button.
 		MESSAGE_BUTTON_YES_NO = MESSAGE_BUTTON_YES | MESSAGE_BUTTON_NO,
+		/// @brief A "YES" button with a "NO" and a "CANCEL" button.
 		MESSAGE_BUTTON_YES_NO_CANCEL = MESSAGE_BUTTON_YES_NO | MESSAGE_BUTTON_CANCEL
 
 	};
 	
+	/// @brief Defines possible display styles of a message box.
+	/// @note On some OSes this affects what icon is displayed within the message box.
 	enum MessageBoxStyle
 	{
+		/// @brief The standard display.
 		MESSAGE_STYLE_NORMAL = 0,
+		/// @brief Special informational display.
 		MESSAGE_STYLE_INFO = 1,
+		/// @brief Display as warning.
 		MESSAGE_STYLE_WARNING = 2,
+		/// @brief Display as critical.
 		MESSAGE_STYLE_CRITICAL = 3,
+		/// @brief Display as question.
 		MESSAGE_STYLE_QUESTION = 4,
+		/// @brief Force modal message box.
+		/// @note Modal means that the user cannot interact with the underlying window until the message box is closed.
+		/// @note Not available on all OSes and some OSes have always-modal message boxes.
 		MESSAGE_STYLE_MODAL = 8,
+		/// @brief Close underlying application before showing the message box.
+		/// @note This might not work the same way in all OSes.
 		MESSAGE_STYLE_TERMINATE_ON_DISPLAY = 16
 
 	};
 
-	DEPRECATED_ATTRIBUTE static const MessageBoxButton AMSGBTN_NULL = (MessageBoxButton)NULL;
-	DEPRECATED_ATTRIBUTE static const MessageBoxButton AMSGBTN_OK = MESSAGE_BUTTON_OK;
-	DEPRECATED_ATTRIBUTE static const MessageBoxButton AMSGBTN_CANCEL = MESSAGE_BUTTON_CANCEL;
-	DEPRECATED_ATTRIBUTE static const MessageBoxButton AMSGBTN_YES = MESSAGE_BUTTON_YES;
-	DEPRECATED_ATTRIBUTE static const MessageBoxButton AMSGBTN_NO = MESSAGE_BUTTON_NO;
-	DEPRECATED_ATTRIBUTE static const MessageBoxButton AMSGBTN_OKCANCEL = MESSAGE_BUTTON_OK_CANCEL;
-	DEPRECATED_ATTRIBUTE static const MessageBoxButton AMSGBTN_YESNO = MESSAGE_BUTTON_YES_NO;
-	DEPRECATED_ATTRIBUTE static const MessageBoxButton AMSGBTN_YESNOCANCEL = MESSAGE_BUTTON_YES_NO_CANCEL;
-
-	DEPRECATED_ATTRIBUTE static const MessageBoxStyle AMSGSTYLE_PLAIN = MESSAGE_STYLE_NORMAL;
-	DEPRECATED_ATTRIBUTE static const MessageBoxStyle AMSGSTYLE_INFORMATION = MESSAGE_STYLE_INFO;
-	DEPRECATED_ATTRIBUTE static const MessageBoxStyle AMSGSTYLE_WARNING = MESSAGE_STYLE_WARNING;
-	DEPRECATED_ATTRIBUTE static const MessageBoxStyle AMSGSTYLE_CRITICAL = MESSAGE_STYLE_CRITICAL;
-	DEPRECATED_ATTRIBUTE static const MessageBoxStyle AMSGSTYLE_QUESTION = MESSAGE_STYLE_QUESTION;
-	DEPRECATED_ATTRIBUTE static const MessageBoxStyle AMSGSTYLE_MODAL = MESSAGE_STYLE_MODAL;
-	DEPRECATED_ATTRIBUTE static const MessageBoxStyle AMSGSTYLE_TERMINATEAPPONDISPLAY = MESSAGE_STYLE_TERMINATE_ON_DISPLAY;
-		
+	/// @brief Get current OS's info.
+	/// @return Current OS's info.
 	aprilFnExport SystemInfo getSystemInfo();
+	/// @brief Get current app's "package name".
+	/// @return Current app's "package name".
+	/// @note This is not available on all platforms.
+	/// @note "Package name" might be named differently on different OSes. e.g. Android names it "Package Name", iOS names it "Bundle ID", WinRT names it "Package Family Name".
 	aprilFnExport hstr getPackageName();
+	/// @brief Get current OS's user directory path.
+	/// @return Current OS's user directory path.
 	aprilFnExport hstr getUserDataPath();
+	/// @brief Displays an OS message box.
+	/// @param[in] title Text displayed in the title bar.
+	/// @param[in] text Text displayed within the message box.
+	/// @param[in] buttonMask Buttons which should be displayed.
+	/// @param[in] style Style of the message box.
+	/// @param[in] customButtonTitles Custom texts for the buttons.
+	/// @param[in] callback Callback function to call once the message box was dismissed. Can be NULL if not needed.
+	/// @note "customButtonTitles" may not work on all OSes.
+	/// @note Depending on the OS, this function may return immediately. Write your code to assume that this function is asynchronous to avoid problems.
+	/// @note Expect that "callback" will be called asynchrously once the message box is dismissed.
 	aprilFnExport void messageBox(chstr title, chstr text, MessageBoxButton buttonMask = MESSAGE_BUTTON_OK, MessageBoxStyle style = MESSAGE_STYLE_NORMAL,
 		hmap<MessageBoxButton, hstr> customButtonTitles = hmap<MessageBoxButton, hstr>(), void(*callback)(MessageBoxButton) = NULL);
 
+	/// @brief Used internally only.
 	void _makeButtonLabels(hstr* ok, hstr* yes, hstr* no, hstr* cancel,
 		MessageBoxButton buttonMask, hmap<MessageBoxButton, hstr> customButtonTitles);
-
+	/// @brief Used internally only.
 	void messageBox_platform(chstr title, chstr text, MessageBoxButton buttonMask = MESSAGE_BUTTON_OK, MessageBoxStyle style = MESSAGE_STYLE_NORMAL,
 		hmap<MessageBoxButton, hstr> customButtonTitles = hmap<MessageBoxButton, hstr>(), void(*callback)(MessageBoxButton) = NULL);
 
