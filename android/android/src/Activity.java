7f51996c0de3d870138dd449ad55ff62fad86bba
Boris Mikic <boris.blizzard@gmail.com>
2015-06-30 11:56:00 +0000

    - fix for potential fullscreen/display problems

--- android/android/src/Activity.java
+++ android/android/src/Activity.java
@@ -110,6 +110,20 @@ public class Activity extends android.app.Activity
 				}
 			});
 		}
+		else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN)
+		{
+			this.getWindow().getDecorView().setSystemUiVisibility(
+				View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+				View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+				View.SYSTEM_UI_FLAG_FULLSCREEN | 
+				View.SYSTEM_UI_FLAG_LOW_PROFILE);
+		}
+		else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+		{
+			this.getWindow().getDecorView().setSystemUiVisibility(
+				View.SYSTEM_UI_FLAG_FULLSCREEN | 
+				View.SYSTEM_UI_FLAG_LOW_PROFILE);
+		}
 		NativeInterface.activityOnCreate();
 	}
 	
@@ -117,11 +131,12 @@ public class Activity extends android.app.Activity
 	{
 		if (this.isEnabledNavigationBarHiding())
 		{
-			this.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
-				View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+			this.getWindow().getDecorView().setSystemUiVisibility(
+				View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
 				View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
-				View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+				View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
 				View.SYSTEM_UI_FLAG_FULLSCREEN |
+				View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
 				View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 		}
 	}
