ecf9f2bae4026685ccfd276e9b0155760799cc32
boris.blizzard <boris.blizzard@gmail.com>
2015-10-19 13:49:05 +0200

    - added dummy files

---
+++ aprilpix/include/aprilpix/ImageWebp.h
@@ -0,0 +1,27 @@
+/// @file
+/// @version 1.0
+/// 
+/// @section LICENSE
+/// 
+/// This program is free software; you can redistribute it and/or modify it under
+/// the terms of the BSD license: http://opensource.org/licenses/BSD-3-Clause
+/// 
+/// @section DESCRIPTION
+/// 
+/// Defines all functions for April PIX.
+
+#ifndef APRILPIX_IMAGE_WEBP_H
+#define APRILPIX_IMAGE_WEBP_H
+
+#include <hltypes/harray.h>
+#include <hltypes/hstring.h>
+
+#include "aprilpixExport.h"
+
+namespace aprilpix
+{
+
+};
+
+#endif
+
---
+++ aprilpix/src/ImagePvr.cpp
@@ -0,0 +1,14 @@
+/// @file
+/// @version 1.0
+/// 
+/// @section LICENSE
+/// 
+/// This program is free software; you can redistribute it and/or modify it under
+/// the terms of the BSD license: http://opensource.org/licenses/BSD-3-Clause
+
+#include "aprilpix.h"
+
+namespace aprilpix
+{
+
+}
