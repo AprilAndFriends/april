1940b4e14ec34b68ab15143b91a944a17cf014f0
Kresimir Spes <kresimir.spes@gmail.com>
2015-10-26 13:41:37 +0100

    egl fix

--- src/egl.cpp
+++ src/egl.cpp
@@ -77,13 +77,9 @@ namespace april
 			}
 			else if (april::rendersys->getName() == APRIL_RS_OPENGLES2)
 			{
-				this->pi32ConfigAttribs[0] = EGL_SURFACE_TYPE;
-				this->pi32ConfigAttribs[1] = EGL_WINDOW_BIT;
-				this->pi32ConfigAttribs[2] = EGL_RENDERABLE_TYPE;
-				this->pi32ConfigAttribs[3] = EGL_OPENGL_ES2_BIT;
-				//this->pi32ConfigAttribs[4] = EGL_RENDERABLE_TYPE;
-				//this->pi32ConfigAttribs[5] = EGL_OPENGL_ES2_BIT;
-				//this->pi32ConfigAttribs[6] = EGL_NONE;
+				this->pi32ConfigAttribs[4] = EGL_RENDERABLE_TYPE;
+				this->pi32ConfigAttribs[5] = EGL_OPENGL_ES2_BIT;
+				this->pi32ConfigAttribs[6] = EGL_NONE;
 			}
 			result = eglChooseConfig(this->display, this->pi32ConfigAttribs, &this->config, 1, &configs);
 			if (!result || configs == 0)
@@ -95,11 +91,12 @@ namespace april
 		}
 		if (this->surface == NULL)
 		{
+			// hWnd is assigned outside of this code on some non-win32 platforms, so we have to use it.
 #if defined(_WIN32) && !defined(_WINRT)
 			this->hWnd = (EGLNativeWindowType)window->getBackendId();
+#endif
 			this->surface = eglCreateWindowSurface(this->display, this->config, this->hWnd, NULL);
 			if (this->surface == NULL)
-#endif
 			{
 				this->surface = eglCreateWindowSurface(this->display, this->config, NULL, NULL);
 			}
