3cbecdc2d7cd4d39adaa250c19aaae031f8f54de
boris.blizzard <boris.blizzard@gmail.com>
2015-10-28 15:47:23 +0100

    - added dummy shaders

---
+++ src/rendersystems/DirectX/11/default-shaders/PixelShader_ColoredTexturedLerp.hlsl
@@ -0,0 +1,12 @@
+/// @version 3.7
+
+#include "Shaders.hlsli"
+
+Texture2D<min16float4> cTexture : register(t0);
+SamplerState cSampler : register(s0);
+
+min16float4 main(PixelShaderInput input) : SV_Target
+{
+	min16float4 tex = cTexture.Sample(cSampler, input.tex);
+	return min16float4(lerp(tex.rgb, input.color.rgb, input.lerpAlpha.a), tex.a * input.color.a);
+}
