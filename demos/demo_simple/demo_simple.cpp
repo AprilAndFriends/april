39dcf5ba5d25b4356d023c27a3a5b60449ed477e
Boris Mikic <boris.blizzard@gmail.com>
2015-11-07 16:18:44 +0100

    - fixed DX9 system-color problem on some drivers that don't support D3DTSS_CONSTANT - added proper Win7+ support for touch input (not supporting multi-touch at the moment)

--- demos/demo_simple/demo_simple.cpp
+++ demos/demo_simple/demo_simple.cpp
@@ -8,11 +8,7 @@
 
 #ifndef _ANDROID
 #ifndef _WINRT
-#ifdef _DEBUG
 #define RESOURCE_PATH "../../demos/media/"
-#else
-#define RESOURCE_PATH "media/"
-#endif
 #else
 #define RESOURCE_PATH "media/"
 #endif
