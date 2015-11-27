44ca3b78259897200da1010aa3abb3ca28bbcecc
Boris Mikic <boris.blizzard@gmail.com>
2015-11-27 12:32:16 +0100

    - small update

--- src/rendersystems/DirectX/11/default-shaders/PixelShader_TexturedAlphaMap.hlsl
+++ src/rendersystems/DirectX/11/default-shaders/PixelShader_TexturedAlphaMap.hlsl
@@ -4,6 +4,5 @@
 
 min16float4 main(PixelShaderInput input) : SV_Target
 {
-	min16float4 tex = cTexture.Sample(cSampler, input.tex);
-	return min16float4(input.color.rgb, input.color.a * tex.r);
+	return min16float4(input.color.rgb, input.color.a * cTexture.Sample(cSampler, input.tex).r);
 }
