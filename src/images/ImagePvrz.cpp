41b583223fc1b2a064b9ea680d4c58e7fe3a39e9
Boris Mikic <boris.blizzard@gmail.com>
2017-01-24 13:53:36 +0100

    - removed debug code

--- src/images/ImagePvrz.cpp
+++ src/images/ImagePvrz.cpp
@@ -26,27 +26,6 @@
 #include "Platform.h"
 #include "zlibUtil.h"
 
-#define READ_LITTLE_ENDIAN_UINT32(location, offset) (location[offset] | (location[offset + 1] << 8) | (location[offset + 2] << 16) | (location[offset + 3] << 24))
-
-#ifndef PVR_Texture_Header
-struct PVR_Texture_Header
-{
-	uint32_t dwHeaderSize;
-	uint32_t dwHeight;
-	uint32_t dwWidth;
-	uint32_t dwMipMapCount;
-	uint32_t dwpfFlags;
-	uint32_t dwTextureDataSize;
-	uint32_t dwBitCount;
-	uint32_t dwRBitMask;
-	uint32_t dwGBitMask;
-	uint32_t dwBBitMask;
-	uint32_t dwAlphaBitMask;
-	uint32_t dwPVR;
-	uint32_t dwNumSurfs;
-};
-#endif
-
 namespace april
 {
 	struct PvrzHeader
@@ -75,26 +54,6 @@ namespace april
 		{
 			return NULL;
 		}
-		struct PVR_Texture_Header pvrHeader;
-		harray<unsigned char> pvrDataArray;
-		for_iter (i, 0, 12)
-		{
-			pvrDataArray += pvrData[i];
-		}
-		int a = 0;
-		pvrHeader.dwHeaderSize = READ_LITTLE_ENDIAN_UINT32(pvrData, 0);
-		pvrHeader.dwHeight = READ_LITTLE_ENDIAN_UINT32(pvrData, 4);
-		pvrHeader.dwWidth = READ_LITTLE_ENDIAN_UINT32(pvrData, 8);
-		pvrHeader.dwMipMapCount = READ_LITTLE_ENDIAN_UINT32(pvrData, 12);
-		pvrHeader.dwpfFlags = READ_LITTLE_ENDIAN_UINT32(pvrData, 16);
-		pvrHeader.dwTextureDataSize = READ_LITTLE_ENDIAN_UINT32(pvrData, 20);
-		pvrHeader.dwBitCount = READ_LITTLE_ENDIAN_UINT32(pvrData, 24);
-		pvrHeader.dwRBitMask = READ_LITTLE_ENDIAN_UINT32(pvrData, 28);
-		pvrHeader.dwGBitMask = READ_LITTLE_ENDIAN_UINT32(pvrData, 32);
-		pvrHeader.dwBBitMask = READ_LITTLE_ENDIAN_UINT32(pvrData, 36);
-		pvrHeader.dwAlphaBitMask = READ_LITTLE_ENDIAN_UINT32(pvrData, 40);
-		pvrHeader.dwPVR = READ_LITTLE_ENDIAN_UINT32(pvrData, 44);
-		pvrHeader.dwNumSurfs = READ_LITTLE_ENDIAN_UINT32(pvrData, 48);
 		Image* image = new Image();
 		image->w = header.width;
 		image->h = header.height;
