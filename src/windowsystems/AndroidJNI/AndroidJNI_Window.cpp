49d65e12485a6a08aba8db7cac22d94e38aac256
Boris Mikic <boris.blizzard@gmail.com>
2017-04-06 21:48:33 +0200

    - small fix

--- src/windowsystems/AndroidJNI/AndroidJNI_Window.cpp
+++ src/windowsystems/AndroidJNI/AndroidJNI_Window.cpp
@@ -6,6 +6,8 @@
 /// This program is free software; you can redistribute it and/or modify it under
 /// the terms of the BSD license: http://opensource.org/licenses/BSD-3-Clause
 
+#include <stdlib.h>
+
 #ifdef _ANDROIDJNI_WINDOW
 #include <jni.h>
 
