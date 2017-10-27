06b6f11db207d54b95e2dd236cc0a0136d6449cb
Boris Mikic <boris.blizzard@gmail.com>
2017-10-27 10:15:54 +0200

    - added setlocale() call to allow better handling of text on all locales

--- include/april/Android_main.h
+++ include/april/Android_main.h
@@ -27,7 +27,7 @@
 /// @note Don't worry about this, this is how Android/Java/JNI has to initialize things.
 extern "C" JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void* reserved)
 {
-	setlocale(LC_ALL, ""); // make sure the app uses a neutral locale that includes all specifics for all locales
+	setlocale(LC_ALL, "en"); // make sure the app uses a neutral locale that includes all specifics for all locales
 	return april::__JNI_OnLoad(&april_init, &april_destroy, vm, reserved);
 }
 #endif
