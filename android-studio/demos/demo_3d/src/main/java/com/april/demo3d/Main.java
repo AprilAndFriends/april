ff322f6c8a08ff515c8010f5d248e5f7b45d6009
Boris Mikic <boris.blizzard@gmail.com>
2018-04-06 14:43:30 +0200

    - android studio update

---
+++ android-studio/demos/demo_3d/src/main/java/com/april/demo3d/Main.java
@@ -0,0 +1,17 @@
+package com.april.demo3d;
+
+public class Main extends com.april.Activity
+{
+	static
+	{
+		System.loadLibrary("demo_3d");
+	}
+	
+	@Override
+	protected void onCreate(android.os.Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		this.forceArchivePath(com.april.NativeInterface.apkPath); // forces APK as archive file
+	}
+	
+}
---
+++ android-studio/demos/demo_pix/src/main/java/com/april/demoPix/Main.java
@@ -0,0 +1,17 @@
+package com.april.demoPix;
+
+public class Main extends com.april.Activity
+{
+	static
+	{
+		System.loadLibrary("demo_pix");
+	}
+	
+	@Override
+	protected void onCreate(android.os.Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		this.forceArchivePath(com.april.NativeInterface.apkPath); // forces APK as archive file
+	}
+	
+}
