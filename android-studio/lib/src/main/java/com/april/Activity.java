d39e95f07a97eb2d5db5c0d1e6d912bd8ac5edb8
Boris Mikic <boris.blizzard@gmail.com>
2019-09-18 16:09:27 +0200

    - android fix

--- android-studio/lib/src/main/java/com/april/Activity.java
+++ android-studio/lib/src/main/java/com/april/Activity.java
@@ -311,18 +311,17 @@ public class Activity extends android.app.Activity implements IActivityEvents
 	
 	protected void hideNavigationBar()
 	{
-		View decorView = this.getWindow().getDecorView();
-		int systemUiVisibility =
-			View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
-			View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
-			View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
-			View.SYSTEM_UI_FLAG_FULLSCREEN |
-			View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+		// do not change this, it can cause issues with certain Android versions
 		if (this.isEnabledNavigationBarHiding())
 		{
-			systemUiVisibility |= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+			this.getWindow().getDecorView().setSystemUiVisibility(
+				View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+				View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+				View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+				View.SYSTEM_UI_FLAG_FULLSCREEN |
+				View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+				View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 		}
-		decorView.setSystemUiVisibility(systemUiVisibility);
 	}
 	
 	protected void registerSensorEventListener()
